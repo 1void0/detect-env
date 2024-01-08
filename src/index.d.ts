0459dd834b7787149f9986821204c5fd3f4cdd27
Cédric Trigoso <cedric@niriu.com>
2024-01-08 22:46:46 +0100

    build: add type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+declare const isBrowser: boolean;
+declare const isNode: boolean;
+declare const isServer: boolean;
+declare const isServerComponent: boolean;
+declare const isRSC: boolean;
+
+export { isBrowser, isNode, isServer, isServerComponent, isRSC };
