c59330ac1883ad31470cc461442961ba502c695e
Cédric Trigoso <cedric@niriu.com>
2024-04-10 11:15:10 +0200

    feat: make browser.client.js commonjs compatible

--- src/browser.client.js
+++ src/browser.client.js
@@ -1,5 +1,18 @@
-export const isBrowser = true;
-export const isNode = false;
-export const isServer = isNode;
-export const isServerComponent = false;
-export const isRSC = isServerComponent;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+
+exports.isServerComponent =
+  exports.isServer =
+  exports.isRSC =
+  exports.isNode =
+  exports.isBrowser =
+    void 0;
+
+const isBrowser = (exports.isBrowser = true);
+const isNode = (exports.isNode = false);
+const isServer = (exports.isServer = isNode);
+const isServerComponent = (exports.isServerComponent = false);
+const isRSC = (exports.isRSC = isServerComponent);
