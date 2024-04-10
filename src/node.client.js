6c6fb9f0d8f13a2fc028cb348857c705b8c438ed
Cédric Trigoso <cedric@niriu.com>
2024-04-10 14:22:10 +0200

    feat: make node.client.js commonjs compatible

--- src/node.client.js
+++ src/node.client.js
@@ -1,5 +1,18 @@
-export const isBrowser = false;
-export const isNode = true;
-export const isServer = isNode;
-export const isServerComponent = false;
-export const isRSC = isServerComponent;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true,
+});
+
+exports.isServerComponent =
+  exports.isServer =
+  exports.isRSC =
+  exports.isNode =
+  exports.isBrowser =
+    void 0;
+
+const isBrowser = (exports.isBrowser = false);
+const isNode = (exports.isNode = true);
+const isServer = (exports.isServer = isNode);
+const isServerComponent = (exports.isServerComponent = false);
+const isRSC = (exports.isRSC = isServerComponent);
